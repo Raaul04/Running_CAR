98ed8b98542bec7d7ff1fbe8a1a3327416d9e201
Raaul04 <raulletrado@hotmail.com>
2023-10-20 12:00:15 +0200

    Camion

--- src/Vehiculos/Camion.java
+++ src/Vehiculos/Camion.java
@@ -1,20 +1,28 @@
 package Vehiculos;
 
 public class Camion extends Vehiculo {
-    private int posicion;
-    public Camion() {
-        super(15); // Velocidad constante del camión
-        posicion=0;
+    private int carrilActual;
+
+    public Camion(int velocidadInicial) {
+        super(velocidadInicial);
+        // Inicialmente, los camiones pueden estar en cualquier carril (1, 2 o 3).
+        this.carrilActual = (int) (Math.random() * 3) + 1;
     }
 
     @Override
     public void esquivarObstaculo() {
-        // Implementación para actuar como un obstáculo (sin esquivar obstáculos).
-        System.out.println("¡Camión en la posicion "+ posicion+"  debes esquivarlo");
-        //Actualiza la posicion
-        System.out.println("Camion esquivado con exito");
+        // Los camiones cambian aleatoriamente de carril para dificultar el adelantamiento.
+        int nuevoCarril;
+        do {
+            nuevoCarril = (int) (Math.random() * 3) + 1;
+        } while (nuevoCarril == carrilActual);
+        cambiarDeCarril(nuevoCarril);
     }
-    public void avanzar(){
-        posicion++;
+
+    public void cambiarDeCarril(int nuevoCarril) {
+        if (nuevoCarril != carrilActual) {
+            System.out.println("Camion cambió del carril " + carrilActual + " al carril " + nuevoCarril);
+            carrilActual = nuevoCarril;
+        }
     }
 }
