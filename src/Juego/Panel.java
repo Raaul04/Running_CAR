bc3d260bfc329de6b6977bb892e1556a43dd7d83
Raaul04 <raulletrado@hotmail.com>
2023-10-20 12:02:06 +0200

    Panel

--- src/Juego/Panel.java
+++ src/Juego/Panel.java
@@ -1,11 +1,14 @@
+package Juego;
 import javax.swing.JPanel;
 import java.awt.Graphics;
+
 
 public class Panel extends JPanel {
     private JPanel panel;
 
     public void paintComponent(Graphics g){
-        super.paintComponent(g);//limpiar cual mujer
+
+        super.paintComponent(g);
 
         g.fillRect(100,100,10,10);
     }
