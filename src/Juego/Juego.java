3b0ab15487bd448e44bc4d056c8858bc67b2f574
Raaul04 <raulletrado@hotmail.com>
2023-10-20 12:01:26 +0200

    Juego

---
+++ src/Juego/Juego.java
@@ -0,0 +1,55 @@
+package Juego;
+
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Juego extends JPanel {
+    Player player= new Player();
+    public static boolean haChocado=false;
+    Trafico vehiculos;
+
+    Player coche=new Player();
+
+    public Juego(){
+        JFrame miVentana = new JFrame("Running Car");
+        Juego game = new Juego();
+        miVentana.add(game);
+        miVentana.setSize(500,900);
+        miVentana.setVisible(true);
+        miVentana.setResizable(false);
+        miVentana.setLocationRelativeTo(null);
+        miVentana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        /** while(true){
+         if(haChocado){
+         int reinicia=JOptionPane.showConfirmDialog(null,"has perdido" +
+         "quieres reiniciar?", JOptionPane.YES_NO_OPTION);
+         if(reinicia==0){
+         reiniciaValores();
+         }else if(reinicia==1){
+         System.exit(0);
+         }
+         }
+         }*/
+        addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                coche.keyPressed(e);
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+
+            }
+        });
+        setFocusable(true);
+        vehiculos = new Trafico(this);
+    }
+
+}
