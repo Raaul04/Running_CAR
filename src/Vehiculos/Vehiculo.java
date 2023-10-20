72f1f0f9b7344e6683395e60908e39494e2c6762
Raaul04 <raulletrado@hotmail.com>
2023-10-20 12:02:40 +0200

    Vehiculo

--- src/Vehiculos/Vehiculo.java
+++ src/Vehiculos/Vehiculo.java
@@ -35,6 +35,17 @@ public abstract class Vehiculo {
     public abstract void esquivarObstaculo();
 
     /**
+     * Método para detectar colisión con otro vehículo.
+     *
+     * @param otroVehiculo El vehículo con el que se verifica la colisión.
+     * @return `true` si hay colisión, `false` de lo contrario.
+     */
+    public boolean colisionCon(Vehiculo otroVehiculo) {
+        int distancia = Math.abs(this.posicionX - otroVehiculo.getPosicionX());
+        return distancia < 20; // Ajusta este valor según la lógica de colisión deseada
+    }
+
+    /**
      * Devuelve una representación en forma de cadena del objeto que incluye el nombre de la clase
      * y la posición actual del vehículo.
      *
