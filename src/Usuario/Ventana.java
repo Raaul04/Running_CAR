ee67d34e3da26669015087280e02f53af7e94539
Raaul04 <raulletrado@hotmail.com>
2023-10-20 16:10:10 +0200

    Ventana

---
+++ src/Usuario/Ventana.java
@@ -0,0 +1,28 @@
+package Usuario;
+
+import Funcionamiento.Juego;
+import Funcionamiento.Panel;
+
+import javax.swing.JFrame;
+
+/**
+ * La clase Ventana representa la ventana principal del juego y contiene el panel de juego y al jugador.
+ */
+public class Ventana extends JFrame {
+    private JFrame ventana;
+    private Juego juego;
+    private Player player;
+
+    /**
+     * Constructor para crear una nueva instancia de Ventana.
+     *
+     * @param panel El panel de juego que se agregará a la ventana.
+     */
+    public Ventana(Panel panel) {
+        ventana = new JFrame();
+        ventana.setSize(400, 400); // Tamaño de la ventana de juego
+        ventana.add(panel);
+        ventana.setVisible(true);
+        player = new Player();
+    }
+}
