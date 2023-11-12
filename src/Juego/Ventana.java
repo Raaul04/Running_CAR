8c7b9d3421e9bf8eb9af42054d50f8d566df8c0d
Raaul04 <raulletrado@hotmail.com>
2023-11-12 22:36:23 +0100

    Update

--- src/Juego/Ventana.java
+++ src/Juego/Ventana.java
@@ -6,13 +6,13 @@ import java.awt.*;
 public class Ventana extends JFrame implements Runnable{
     private boolean running = false;
     private final int FPS = 60; //cantidad de fotogramas por segundo a la que queremos que funcione el juego,
-                                //60 ya que para este tipo de juego creemos que es lo adecuado
+    //60 ya que para este tipo de juego creemos que es lo adecuado
     private double tiempoRequerido = 1000000000/FPS; //tiempo requerido para cada FPS, usando nanosegundos ya que es mas preciso
     private double delta = 0; //almacena temporalmente el tiempo que va pasando
     private int averageFPS = FPS; //promedio de FPS para ver el numero de FPS en los que estamos ya que dependiendo de la accion estos pueden subir o bajar
 
     private Juego juego;
-    private Coche_Player coche; //referencia al Coche_Player
+    private Coche_Player coche;
 
     public Ventana(Panel panel) {
         setSize(400, 400); // Tamaño de la ventana de juego
