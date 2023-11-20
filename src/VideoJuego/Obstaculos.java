de59b82582a0dde2b2e649778b9a199e50868ec1
Raaul04 <raulletrado@hotmail.com>
2023-11-20 10:03:08 +0100

    Update

--- src/VideoJuego/Obstaculos.java
+++ src/VideoJuego/Obstaculos.java
@@ -2,9 +2,21 @@ package VideoJuego;
 
 import java.awt.*;
 
+/**
+ * La clase Obstaculos representa un obstáculo en un videojuego. Cada obstáculo tiene una posición (x, y)
+ * y dimensiones (ancho y alto) que determinan su área en el espacio del juego.
+ */
 public class Obstaculos {
     private int x, y, width, height;
 
+    /**
+     * Construye un nuevo objeto Obstaculos con la posición y dimensiones especificadas.
+     *
+     * @param x      La coordenada x del obstáculo.
+     * @param y      La coordenada y del obstáculo.
+     * @param width  El ancho del obstáculo.
+     * @param height El alto del obstáculo.
+     */
     public Obstaculos(int x, int y, int width, int height) {
         this.x = x;
         this.y = y;
@@ -12,8 +24,12 @@ public class Obstaculos {
         this.height = height;
     }
 
+    /**
+     * Devuelve un objeto Rectangle que representa el área delimitada por el obstáculo.
+     *
+     * @return Un objeto Rectangle que representa el área del obstáculo.
+     */
     public Rectangle getBounds() {
         return new Rectangle(x, y, width, height);
     }
 }
-
